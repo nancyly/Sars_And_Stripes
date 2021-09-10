bafec4ba7c3544ff413dc2bd0fa7159bea08a853
Zachary <zackboutchyard@gmail.com>
2021-09-10 01:47:55 -0400

    Commit page 1,2,3

--- app/src/main/java/net/androidbootcamp/sars_and_stripes/MainActivity.java
+++ app/src/main/java/net/androidbootcamp/sars_and_stripes/MainActivity.java
@@ -2,13 +2,28 @@ package net.androidbootcamp.sars_and_stripes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCreate = (Button) findViewById(R.id.btnCreate);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, SignUp.class));
+
+            }
+        });
     }
 }
